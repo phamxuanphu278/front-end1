7a77dd1c87bb29af45dba734972e6d51c89640c3
phamxuanphu278 <phuphamxuan278@gmail.com>
2020-09-09 12:42:17 +0700

    commit

--- js/main-jquery.js
+++ js/main-jquery.js
@@ -1,45 +1,14 @@
 jQuery(document).ready( function($){
-  var swiper = new Swiper('.swiper-container', {
+  var swiper = new Swiper('.swiper-sl1', {
         loop: true,
         autoplay: {
               delay: 3000,
               disableOnInteraction: false,
       },
         navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
+          nextEl: '.next-buttons',
+          prevEl: '.prev-buttons',
         },
-    });
-    var mySwiper = new Swiper('.swip-slide1', {
-      slidesPerView: 3,
-        autoplay: {
-            delay: 2500,
-            disableOnInteraction: false
-        },
-        pagination: {
-            el: '.swiper-pagination',
-            type: 'progressbar',
-        },
-        breakpoints: {
-            480: {
-                slidesPerView: 1,
-                spaceBetween: 10,
-            },
-            768: {
-                slidesPerView: 1,
-                spaceBetween: 10,
-
-            },
-            992: {
-                slidesPerView: 2,
-                spaceBetween: 40,
-            },
-            1200: {
-                slidesPerView: 3,
-                spaceBetween: 25,
-            },
-        }
-  
     });
 });
 
--- js/main.js
+++ js/main.js
@@ -56,32 +56,84 @@ function myFunction() {
     var content1 = document.getElementById("content1");
     var content2 = document.getElementById("content2");
     var content3 = document.getElementById("content3");
+    var content4 = document.getElementById("content4");
 
     var btnTab1 = document.getElementById("btn-tab1");
     var btnTab2 = document.getElementById("btn-tab2");
     var btnTab3 = document.getElementById("btn-tab3");
+    var btnTab4 = document.getElementById("btn-tab4");
 
-    function openHTML() {
+    function openAsia() {
       content1.style.transform = "translateX(0)";
       content2.style.transform = "translateX(100%)";
       content3.style.transform = "translateX(100%)";
-      btnTab1.style.color = "#ff7846";
+      content4.style.transform = "translateX(100%)";
+      btnTab1.style.color = "#d45a5a";
       btnTab2.style.color = "#000";
       btnTab3.style.color = "#000";
+      btnTab4.style.color = "#000";
     }
-    function openCSS() {
+    function openEurope() {
       content1.style.transform = "translateX(100%)";
       content2.style.transform = "translateX(0)";
       content3.style.transform = "translateX(100%)";
+      content4.style.transform = "translateX(100%)";
       btnTab1.style.color = "#000";
-      btnTab2.style.color = "#ff7846";
+      btnTab2.style.color = "#d45a5a";
       btnTab3.style.color = "#000";
+      btnTab4.style.color = "#000";
     }
-    function openJS() {
+    function openAmerica() {
       content1.style.transform = "translateX(100%)";
       content2.style.transform = "translateX(100%)";
       content3.style.transform = "translateX(0)";
+      content4.style.transform = "translateX(100%)";
       btnTab1.style.color = "#000";
       btnTab2.style.color = "#000";
-      btnTab3.style.color = "#ff7846";
+      btnTab3.style.color = "#d45a5a";
+      btnTab4.style.color = "#000";
     }
+    function openAfrica() {
+      content1.style.transform = "translateX(100%)";
+      content2.style.transform = "translateX(100%)";
+      content3.style.transform = "translateX(100%)";
+      content4.style.transform = "translateX(0)";
+      btnTab1.style.color = "#000";
+      btnTab2.style.color = "#000";
+      btnTab3.style.color = "#000";
+      btnTab4.style.color = "#d45a5a";
+    }
+
+    // Multi step form
+
+    var Form1 = document.getElementById("form1");
+		var Form2 = document.getElementById("form2");
+		var Form3 = document.getElementById("form3");
+
+		var Next1 = document.getElementById("next1");
+		var Next2 = document.getElementById("next2");
+		var Back1 = document.getElementById("back1");
+		var Back2 = document.getElementById("back2");
+
+		var Progress = document.getElementById("progress");
+		
+		Next1.onclick = function() {
+			Form1.style.left = "-450px";
+			Form2.style.left = "40px";
+			Progress.style.width = "240px";
+		};
+		Back1.onclick = function() {
+			Form1.style.left = "40px";
+			Form2.style.left = "450px";
+			Progress.style.width = "120px";
+		};
+		Next2.onclick = function() {
+			Form2.style.left = "-450px";
+			Form3.style.left = "40px";
+			Progress.style.width = "360px";
+		};
+		Back2.onclick = function() {
+			Form2.style.left = "40px";
+			Form3.style.left = "450px";
+			Progress.style.width = "240px";
+		}
